b51b8c279c64f21a69f59faf155838ced10cccdf
thisisarchana <archana.arunkumar10@gmail.com>
2026-03-27 17:53:13 +0530

    Fixed the display of ribbon in club page

--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -1,7 +1,13 @@
 "use client"
 
+import { Navbar } from "@/components/navbar"
 import { ClubExplorer } from "@/components/clubs/club-explorer"
 
 export default function ClubsPage() {
-  return <ClubExplorer />
+  return (
+    <>
+      <Navbar />
+      <ClubExplorer />
+    </>
+  )
 }
