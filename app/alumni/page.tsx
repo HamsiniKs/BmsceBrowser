ed6111816a3c79d43dc7755ab0494e86f9e197a1
thisisarchana <archana.arunkumar10@gmail.com>
2026-03-27 16:39:29 +0530

    Added alumni members page

---
+++ app/alumni/page.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "@/components/navbar"
+import { AlumniShowcase } from "@/components/alumni-showcase"
+import { Footer } from "@/components/footer"
+
+export default function AlumniPage() {
+  return (
+    <main className="min-h-screen bg-alabaster">
+      <Navbar variant="solid" />
+      <div className="pt-8">
+        <AlumniShowcase />
+      </div>
+      <Footer />
+    </main>
+  )
+}
